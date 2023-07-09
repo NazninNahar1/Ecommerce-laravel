fc3dab69facc4e5515b1ab5982aa16bb82901c62
NazninNahar1 <naznin@schertech.com>
2023-07-09 12:26:53 +0600

    crud with lodata

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\Model\Product;
 use Illuminate\Support\ServiceProvider;
+use Flat3\Lodata\Facades\Lodata;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Lodata::discover(Product::class);
     }
 }
